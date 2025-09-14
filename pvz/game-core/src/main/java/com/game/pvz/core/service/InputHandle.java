0dc3bb4773360c4460589b8e22291129f225d4da
ForEsee2606 <2606552547@qq.com>
2025-09-14 20:30:27 +0800

    Delete pvz directory

--- pvz/game-core/src/main/java/com/game/pvz/core/service/InputHandle.java
+++
@@ -1,7 +0,0 @@
-package com.game.pvz.core.service;
-
-/**
- * 【接口】输入
- */
-public interface InputHandle {
-}
--- pvz/game-core/src/main/java/com/game/pvz/module/entity/GameObject.java
+++
@@ -1,7 +0,0 @@
-package com.game.pvz.module.entity;
-
-/**
- * 游戏实体标记接口
- */
-public interface GameObject {
-}
